5a04d64108c1b9467762734bed3926f674fa3ae5
Matt Faircloth <mattcfaircloth@gmail.com>
2017-10-31 00:52:04 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,6 +1,6 @@
-def my_hash =  {"first_name" => "Matt", "last_name" => "Faircloth"}
+def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-
+  my_hash =  {"first_name" => "Matt", "last_name" => "Faircloth"}
 
 end
 
